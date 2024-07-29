76f3ff57d3224d88bb2fd3da7a6998e9430f37d3
momonoski55555 <112166484+momonoski55555@users.noreply.github.com>
2024-07-30 00:01:28 +0200

    did a bit of tidying up a bit cleaned some stuff
    planning to make a Design doc soon
    and a design/story

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,35 +1,24 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-var Mass: float
+const SPEED: int = 5.0
+const JUMP_VELOCITY: float = 4.5
 
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-
-
-
+var gravity := 9.5
 
 func _physics_process(delta):
-	# Add the gravity.
+	
 	if not is_on_floor():
-		velocity.y -= gravity  * delta 
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY # - Mass
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("A", "D", "W", "S")
-	var direction = ($Gimble.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.y -= gravity  * delta
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+		var input_dir := Input.get_vector("A", "D", "W", "S")
+		var direction = ($Gimble.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		if direction:
+			velocity = direction * SPEED
+		else:
+			velocity = lerp(velocity, Vector3.ZERO, delta * 3)
+	
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
 	move_and_slide()
